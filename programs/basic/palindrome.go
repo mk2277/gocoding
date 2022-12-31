d578cd12b31793d3469be254f99a62e295929646
mk2277 <mallikarjun.kaithapuram4@gmail.com>
2022-12-30 19:07:54 -0700

    changes in palindrome

--- programs/basic/palindrome.go
+++ programs/basic/palindrome.go
@@ -2,19 +2,22 @@ package basic
 
 import (
 	"fmt"
-	"strconv"
+	// "strconv"
 )
 
 func Palinornot(x int) string {
 
-	a := strconv.Itoa(x)
+	// a := strconv.Itoa(x)
 	y := 0
 	z := x
 
-	for i := 0; i < len(a); i++ {
+	for {
 		z := x % 10
 		y = y*10 + z
 		x = x / 10
+		if x == 0 {
+			break
+		}
 	}
 	fmt.Println(z, y)
 	if z == y {
